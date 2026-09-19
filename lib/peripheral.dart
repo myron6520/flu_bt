1a59bf7d3e5abf9b052eb5bb050470d353cf1e53
myron6520 <100897740+myron6520@users.noreply.github.com>
2026-09-19 14:52:50 +0800

    Update peripheral.dart

--- lib/peripheral.dart
+++ lib/peripheral.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: constant_identifier_names
 
+import 'dart:io';
+
 class Peripheral {
   static const int STATE_DISCONNECTED = 0;
   static const int STATE_CONNECTING = 1;
@@ -21,13 +23,15 @@ class Peripheral {
     uuid = info["uuid"] ?? "";
     rssi = int.tryParse("${info['rssi']}") ?? 0;
     state = int.tryParse("${info['state']}") ?? STATE_DISCONNECTED;
-    deviceType = int.tryParse("${info['deviceType']}") ?? DEVICE_TYPE_UNKNOWN;
+    deviceType = int.tryParse("${info['deviceType']}") ??
+        (Platform.isIOS ? DEVICE_TYPE_LE : DEVICE_TYPE_UNKNOWN);
   }
   void makeValue(Map info) {
     name = info["name"] ?? "";
     uuid = info["uuid"] ?? "";
     rssi = int.tryParse("${info['rssi']}") ?? 0;
     state = int.tryParse("${info['state']}") ?? STATE_DISCONNECTED;
-    deviceType = int.tryParse("${info['deviceType']}") ?? DEVICE_TYPE_UNKNOWN;
+    deviceType = int.tryParse("${info['deviceType']}") ??
+        (Platform.isIOS ? DEVICE_TYPE_LE : DEVICE_TYPE_UNKNOWN);
   }
 }
